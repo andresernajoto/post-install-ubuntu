157be2f1d86ebb0baee519b2deb83921a8b09404
André Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2022-10-01 17:21:05 -0300

    added post-installer file

---
+++ ubuntu-post-installer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Snap Store
+sudo apt install snapd -y
+sudo snap install snap-store
+
+# Socials
+sudo snap install teams-for-linux
+sudo snap install thunderbird
+sudo snap install zoom-client
+
+# Audio
+sudo snap install vlc
+
+# Productivity
+sudo snap install inkscape
+sudo snap install firefox
+sudo snap install onlyoffice-desktopeditors
+sudo snap install code node --classic
