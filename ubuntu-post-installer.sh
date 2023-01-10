d20a5d12a0bdad09fd39de2a178d4995d5eed2ed
André Sernajoto <69870279+andresernajoto@users.noreply.github.com>
2023-01-10 01:22:56 -0300

    updating post-installer

--- ubuntu-post-installer.sh
+++ ubuntu-post-installer.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
-# Snap Store
-sudo apt install snapd -y
-sudo snap install snap-store
-
-# Socials
+# Video Conference
 sudo snap install teams-for-linux
 sudo snap install zoom-client
 
 # Audio
 sudo snap install vlc
-
-# Productivity
-sudo snap install inkscape
-sudo snap install firefox
-sudo snap install onlyoffice-desktopeditors
-sudo snap install code node --classic
 sudo snap install spotify
 
+# Coding
+sudo snap install code node --classic
+sudo snap install postman
+
+# Productivity
+sudo snap install firefox
+sudo snap install onlyoffice-desktopeditors
+
+# Design
+sudo snap install inkscape
 sudo apt update
-sudo apt install neofetch
+sudo apt install neofetch -y
+sudo apt install chrome-gnome-shell https://extensions.gnome.org/extension/3960/transparent-top-bar-adjustable-transparency/
 
 # Gaming
 sudo snap install steam --edge
+
+# VsCode Extensions
+code --install-extension formulahendry.code-runner
+code --install-extension firefox-devtools.vscode-firefox-debug
+code --install-extension ttoowa.in-your-face-incredible
+code --install-extension ritwickdey.liveserver
+code --install-extension esbenp.prettier-vscode
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension tonybaloney.vscode-pets
+code --install-extension ms-dotnettools.csharp
+code --install-extension adrianwilczynski.asp-net-core-switcher
